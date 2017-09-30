2ed5a3c832ceaaeb1d434018134560aa9e54bb2a
nzetzl <nzetzl@purdue.edu>
2017-09-30 17:19:31 -0400

    added initial server code

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ app.use(express.static('public'));
 var http = require('http').Server(app);
 var port = 3000;
 
+//app.get('/', function(req, res) {
+//	app.use(express.static(path.join('BoilerChess/login.html', 'public')));
+
+//});
+
 http.listen(port, function () {
 	console.log("using port # " + port);
 });
+
