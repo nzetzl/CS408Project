46bc5b79af952f4143107b4f0f02a15a2555c2de
skwarte <skwarte@purdue.edu>
2017-09-30 18:59:59 -0400

    setting port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 var http = require('http').Server(app);
-var port = 4000;
+var port = process.env.PORT || 4000;
 
 app.get('/', function(req, res) {
 //	res.sendFile(__dirname + '/BoilerChess/login.html');
