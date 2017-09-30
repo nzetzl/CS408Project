cba66a1946286477eca41c953489f56068215e3f
nzetzl <nzetzl@purdue.edu>
2017-09-30 17:32:32 -0400

    login page is now sent correctly

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 var http = require('http').Server(app);
-var port = 3000;
+var port = 4000;
 
-//app.get('/', function(req, res) {
-//	app.use(express.static(path.join('BoilerChess/login.html', 'public')));
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/BoilerChess/login.html');
 
-//});
+});
 
 http.listen(port, function () {
 	console.log("using port # " + port);
