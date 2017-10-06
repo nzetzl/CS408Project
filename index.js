dbd56970f68de312187838b1e27adfe5c39b77c5
skwarte <skwarte@purdue.edu>
2017-10-06 12:52:08 -0400

    setting up database with sqlite3

--- index.js
+++ index.js
@@ -1,48 +1,29 @@
 var express = require('express');
-var sqlite3 = requre('sqlite3').verbose();
+var sqlite3 = require('sqlite3').verbose();
 var app = express();
+var bodyParser = require('body-parser')
 app.use(express.static('public'));
 var http = require('http').Server(app);
 var port = process.env.PORT || 4000;
 
-app.get('/', function(req, res) {
+app.use(bodyParser.urlencoded({ extended: false }))
+var db = new sqlite3.Database('./users.db');
+//db.
+app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/BoilerChess/login.html');
 });
 
-app.get('/createProfile.html', function(req, res) {
+app.get('/createProfile.html', function (req, res) {
 	res.sendFile(__dirname + '/BoilerChess/createProfile.html');
-	
-});
-/*
-//sql setup
-var sql = require('mssql');
-var sqlConfig = {
-	user: 'login',
-	password: 'pass',
-	server: 'localhost',
-	database: 'TestDB'
-}
 
-app.get('/userProfile.html', function(req, res) {
-	//TODO; implement authentication here
-	console.log("username: " + req.query.username + " \n");
-	console.log("pass: " + req.query.password + " \n");
-	var pass = "";
-	var sqlConnection = new sql.Connection(webconfig, function(err) {
-		var request = new sql.Request(sqlConnection);
-		pass = request.query('SELECT password FROM UsersTable WHERE username = username');
-	});
-	if (pass === req.password) {
-		console.log("correct login credentials. User can proceed.\n");
-		res.sendFile(__dirname + '/BoilerChess/');
-	} else {
-		console.log("incorrect password\n");
-	}
-	res.sendFile(__dirname + '/BoilerChess/userProfile.html');
 });
-*/
 
+app.post('/createProfile.html', function (req, res) {
+	db.all('insert into users (username, pass) values', (err, results) => {
+		console.log(results);
+		res.send(req.body);
+	})
+});
 http.listen(port, function () {
 	console.log("using port # " + port + " \n");
 });
-
