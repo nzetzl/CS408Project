c25e5bfe831c205f72e6fcd5cc8e7c0214cf0f23
nzetzl <nzetzl@purdue.edu>
2017-10-20 20:22:33 -0400

    worked on messenger class in Messenger.js

--- chessGame/Messenger.js
+++ chessGame/Messenger.js
@@ -4,19 +4,32 @@
 //":CHAT:Hello there my good opponent." starting the string with :CHAT: will send the remaining string to the opponents chatbox
 //":MOVE:movestring" starting the string with :MOVE: will send the rest of the string to the opponents move history.
 
-function Messenger(){
-	this.messagesSent = [];
-	this.messagesToSend = [];
-	this.messagesRecieved = [];
-	this.addMessageToSend = function(message){
+class Messenger(){
+	
+	constructor() {
+		var url = "/messages";
+		this.messagesSent = [];
+		this.messagesToSend = [];
+		this.messagesRecieved = [];
+	}
+
+	this.addMessageToSend(message) {
 		if(typeof message === "string"){
 			this.messagesToSend.push(message);
 		}
-	};
-	this.sendMessage = function(){
+	}
+
+	this.sendMessage() {
 		//TODO
+		var http = new XMLHTTPRequest();
+		http.open("POST", url, true);
+		http.send(messagesToSend.toString());
+		//http.send("Test");
 	};
-	this.receiveMessage = function(){
+	this.receiveMessage() {
 		//TODO
+		var http = new XMLHTTPRequest();
+		http.open('GET', url, true);
+		http.send();
 	};
 }
